a48a6f6dac12eb5f5acf71f711e31e008c296d58
Nicholas Stano <nicholasstano@gmail.com>
2019-05-10 14:25:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 function add(a, b) {
-  return (b + a) 
+  return a + b 
 }
+//returns the sum of the inputted values a and b.
 
 function subtract(a, b) {
-  return (a - b)
+  return a - b
 }
+//returns the subtraction of b from a 
 
 function multiply(a, b) {
-  return (a*b)
+  return a*b
 }
+//returns the multiplication of a and b 
 
 function divide(a, b) {
-  return (a / b)
+  return a/b 
 }
+//returns the divison of b by a
 
 function inc(n) {
-  return (n += 1)
+  return n +=1 
 }
+//adds 1 to n and returns the result
 
 function dec(n) {
-   return (n -= 1) 
-  }
+  return n -= 1
+}
+//subtracts 1 from n and returns the result.
 
 function makeInt(n) {
   return parseInt(n, 10)
 }
+//returns n as an integer where n =/= string
 
 function preserveDecimal(n) {
-  return parseFloat(n)
+return parseFloat(n)
 }
+//returns n as a rational number 
